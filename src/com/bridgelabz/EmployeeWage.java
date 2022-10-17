89bc472c8267c46719ac503adbfd34e7f8a7b56f
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-10-18 00:52:41 +0530

    Added: Employee daily wage.

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,12 +4,18 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("*** Welcome to Employee wage computation ***");
 
+        int empHrs=0;
+        int wagePerHr=20;
         int empCheck=(int)(Math.random()*2);
         if(empCheck==1){
             System.out.println("Employee is Present");
+            empHrs=8;
         }
         else{
             System.out.println("Employee is Absent");
+             empHrs=0;
         }
+        int wage= wagePerHr * empHrs;
+        System.out.println("Daily wage of Employee is:" +wage);
     }
 }
