c33b8e66b84db0ad0bbee1d863e28de5e0e6e563
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-10-19 00:28:46 +0530

    Added: Employee wage builder

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,34 +2,36 @@ package com.bridgelabz;
 public class EmployeeWage {
 
     static final int IS_FULL_TIME = 1, IS_PART_TIME = 2, WAGEPERHR = 20, WORKINGDAYS = 20;
+    static final int MAX_HRS_IN_MONTH = 100;
 
     public static void main(String[] args) {
         System.out.println("*** Welcome to Employee wage computation ***");
+        //variables
+        int monthlywage = 0, totalEmpHrs = 0;
+        int empHrs = 0, totalWorkingDays = 0;
 
-        int monthlywage = 0,dailywage;
-        int empHrs = 0;
-        for (int i = 1; i <= WORKINGDAYS; i++) {
+        //Computation
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < WORKINGDAYS) {
+            totalWorkingDays++;
             int empCheck = (int) (Math.random() * 3);
 
             switch (empCheck) {
                 case IS_FULL_TIME:
-                    System.out.println("Employee is Present full time");
+                    // System.out.println("Employee is Present full time");
                     empHrs = 8;
                     break;
                 case IS_PART_TIME:
-                    System.out.println("Employee is Present Part time");
+                    //System.out.println("Employee is Present Part time");
                     empHrs = 4;
                     break;
                 default:
-                    System.out.println("Employee is Absent");
+                    //System.out.println("Employee is Absent");
                     empHrs = 0;
             }
-
-            dailywage = WAGEPERHR * empHrs;
-            monthlywage += dailywage;
-            System.out.println("Employee wage:" +dailywage);
+            totalEmpHrs += empHrs;
+            System.out.println("Day:" + totalWorkingDays + " Employee Hrs " + empHrs);
         }
-
-        System.out.println("Monthly wage of Employee is:" +monthlywage);
+        monthlywage = totalEmpHrs * WAGEPERHR;
+        System.out.println("Total employee wage is: " + monthlywage);
     }
-    }
+}
