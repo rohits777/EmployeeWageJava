76172fc8fa1814e03abbf79f10559a19494440cd
Rohit <Rohit@spotblhol0128.awsspoton.com>
2022-11-16 15:59:28 +0530

    Added: UC9 Employee wage for each company.

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,18 +1,26 @@
 package com.bridgelabz;
 public class EmployeeWage {
 
-    static final int IS_FULL_TIME = 1, IS_PART_TIME = 2, WAGEPERHR = 20, WORKINGDAYS = 20;
-    static final int MAX_HRS_IN_MONTH = 100;
+    public static final int IS_FULL_TIME = 1, IS_PART_TIME = 2;
+      private final String company;
+      private final int wagePerHr;
+      private final int workingDays;
+      private final int maxHrsPerMonth;
+      private int monthlyWage;
+    public  EmployeeWage(String company,int wagePerHr,int maxHrsPerMonth,int workingDays) {
+        this.company = company;
+        this.wagePerHr = wagePerHr;
+        this.workingDays = workingDays;
+        this.maxHrsPerMonth = maxHrsPerMonth;
 
-    public void computeWage(String company,int wagePerHr,int maxHrsPerMonth,int workingDays)
-    {
 
+    }
+    private int computeWage(){
         //variables
-        int monthlywage = 0, totalEmpHrs = 0;
-        int empHrs = 0, totalWorkingDays = 0;
-
+            int totalEmpHrs = 0;
+            int empHrs = 0, totalWorkingDays = 0;
         //Computation
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < WORKINGDAYS) {
+        while (totalEmpHrs <= maxHrsPerMonth && totalWorkingDays < workingDays) {
             totalWorkingDays++;
             int empCheck = (int) (Math.random() * 3);
 
@@ -32,19 +40,22 @@ public class EmployeeWage {
             totalEmpHrs += empHrs;
             System.out.println("Day:" + totalWorkingDays + " Employee present Hrs " + empHrs);
         }
-        monthlywage = totalEmpHrs * WAGEPERHR;
-        System.out.println("Total employee wage of: "+company+ "is:" + monthlywage);
-    }
+      return totalEmpHrs * wagePerHr;
+      //  return "Total employee wage of: "+company+" is:" + monthlywage;
 
+    }
     public static void main(String[] args) {
 
         System.out.println("*** Welcome to Employee wage computation ***");
-                EmployeeWage emp = new EmployeeWage();
-                emp.computeWage("Delhivery",20, 90,19);
-                emp.computeWage("KingsLanding",20,100, 20 );
-                emp.computeWage("MumbaiIndians", 20,95,17);
+        System.out.println("------------------------------------------------------------");
+                EmployeeWage deldivery = new EmployeeWage("Delhivery",20, 90,19);
+                EmployeeWage kings= new EmployeeWage("KingsLanding",20,100, 20);
+                EmployeeWage mumbaiindians= new EmployeeWage("MumbaiIndians", 20,95,17);
 
+        System.out.println("Total employee wage for company" +deldivery.company+ " is:-" +deldivery.computeWage());
+        System.out.println("Total employee wage for company" +kings.company+ " is:-" +kings.computeWage());
+        System.out.println("Total employee wage for company" +mumbaiindians.company+ "is:- " +mumbaiindians.computeWage());
 
-        }
+    }
 
     }
